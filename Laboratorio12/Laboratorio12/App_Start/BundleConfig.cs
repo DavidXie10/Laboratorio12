b8ff584e4fb6e38e74b1ab992b4dec119e17bb64
DavidXie10 <davidxieli@gmail.com>
2021-11-11 12:03:26 -0600

    Adapted bootstrap 5

--- Laboratorio12/Laboratorio12/App_Start/BundleConfig.cs
+++ Laboratorio12/Laboratorio12/App_Start/BundleConfig.cs
@@ -20,8 +20,11 @@ namespace Laboratorio12 {
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new Bundle("~/bundles/bootstrapJS").Include(
+                      "~/Scripts/bootstrap.min.js"));
         }
     }
 }
